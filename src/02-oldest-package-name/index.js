e23bf0d946e3b94dbbe240aee437cc0243d21a22
Alsu3912 <alsu3912@gmail.com>
2021-05-13 17:28:14 +0100

    second task

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -1,35 +1,20 @@
-/**
- * Make the following POST request with either axios or node-fetch:
-
-POST url: http://ambush-api.inyourarea.co.uk/ambush/intercept
-BODY: {
-    "url": "https://api.npms.io/v2/search/suggestions?q=react",
-    "method": "GET",
-    "return_payload": true
-}
-
- *******
-
-The results should have this structure:
-{
-    "status": 200.0,
-    "location": [
-      ...
-    ],
-    "from": "CACHE",
-    "content": [
-      ...
-    ]
-}
-
- ******
-
+/*
  *  With the results from this request, inside "content", return
  *  the "name" of the package that has the oldest "date" value
  */
 
-module.exports = async function oldestPackageName() {
-  // TODO
+const fetchPackageJson = require('../fetchPackageJson');
+const url = require('../fetchParams').url;
+const body = require('../fetchParams').body;
 
-  return name
+module.exports = async function oldestPackageName() {
+  const json = await fetchPackageJson(url, body);
+
+  let packageWithOldestDate = json.content.reduce((acc, cur) => {
+    const oldestDate  = new Date(acc.package.date);
+    const currentItemDate = new Date(cur.package.date); 
+    return oldestDate.getTime() < currentItemDate.getTime() ? acc : cur;
+  });
+
+  return packageWithOldestDate.package.name;
 };
